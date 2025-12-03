6dad1ffb2089554a710be63aef38c96bd04625f7
anpoliros <zzzbc@outlook.com>
2025-12-03 16:16:09 +0800

    Initial Commit

---
+++ FloatingClock/FloatingClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingClockApp.swift
+//  FloatingClock
+//
+//  Created by anpoliros on 2025/12/3.
+//
+
+import SwiftUI
+
+@main
+struct FloatingClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
